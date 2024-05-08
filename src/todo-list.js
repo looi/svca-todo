0ef76a3f6480239191fee0a182fd7c33a8a26e2b
looi <wenlilooi@gmail.com>
2024-05-07 17:50:24 -0700

    fill in blanks

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,27 +1,6 @@
 import { LitElement, html } from 'lit'
 import { repeat } from 'lit/directives/repeat.js';
 
-const LANG_TABLE = {
-  en: {
-    title: 'SVCA To-Do List',
-    language: 'Language',
-    add: 'Add',
-    delete: 'Delete',
-    delete_all: "Delete All",
-    hide_done: "Hide Done",
-    raw_data: "Raw Data",
-  },
-  zh: {
-    title: 'SVCA任务清单',
-    language: '语言',
-    add: '添加',
-    delete: "删除",
-    delete_all: "删除所有",
-    hide_done: "隐藏完成",
-    raw_data: "原始数据",
-  }
-};
-
 export class TodoList extends LitElement {
   static properties = {
     state: {},
@@ -33,7 +12,8 @@ export class TodoList extends LitElement {
   }
 
   render() {
-    const { lang, todos } = this.state;
+    // TODO: make everything work
+    const { todos } = this.state;
     const todo_indices = [];
     todos.forEach((x, i) => {
       if (this.state.hide_done && x.done) return;
@@ -43,26 +23,26 @@ export class TodoList extends LitElement {
       const x = todos[i];
       return html`
         <li>
-          <input type=checkbox .checked=${x.done} @input=${e => this.updTodo(i, 'done', e.target.checked)}>
-          <input .value=${x.text} @input=${e => this.updTodo(i, 'text', e.target.value)}>
-          <button @click=${() => this.delTodo(i)}>${LANG_TABLE[lang].delete}</a>
+          <input type=checkbox>
+          <input>
+          <button>删除</a>
         </li>
       `;
     });
     return html`
-      <h1>${LANG_TABLE[lang].title}</h1>
+      <h1>SVCA任务清单</h1>
       <p>
-      ${LANG_TABLE[lang].language}:
-      <label><input type=radio name=lang .checked=${lang == 'zh'} @input=${e => this.setLang('zh')}>中文</label>
-      <label><input type=radio name=lang .checked=${lang == 'en'} @input=${e => this.setLang('en')}>English</label>
+      语言:
+      <label><input type=radio name=lang>中文</label>
+      <label><input type=radio name=lang>English</label>
       </p>
-      <button @click=${this.addTodo}> ${LANG_TABLE[lang].add}</button>
-      <button @click=${this.delAll}> ${LANG_TABLE[lang].delete_all}</button>
-      <label><input type=checkbox .checked=${this.state.hide_done} @input=${e => this.setHideDone(e.target.checked)}>${LANG_TABLE[lang].hide_done}</label>
+      <button @click=${this.addTodo}>添加</button>
+      <button>删除所有</button>
+      <label><input type=checkbox>隐藏完成</label>
       <ol>
       ${els}
       </ol>
-      <p>${LANG_TABLE[lang].raw_data}: ${JSON.stringify(this.state)}</p>
+      <p>原始数据: ${JSON.stringify(this.state)}</p>
     `
   }
 
@@ -72,28 +52,27 @@ export class TodoList extends LitElement {
   }
 
   updTodo(i, p, x) {
-    this.state.todos[i][p] = x;
+    // TODO_ update todo
     this.requestUpdate();
   }
 
   delTodo(i) {
-    if (!confirm("你为什么要删除我？救命！")) return;
-    this.state.todos.splice(i, 1);
+    // TODO: delete single todo
     this.requestUpdate();
   }
 
   delAll() {
-    this.state.todos = [];
+    // TODO: delete all todos
     this.requestUpdate();
   }
 
   setHideDone(x) {
-    this.state.hide_done = x;
+    // TODO: set hide done
     this.requestUpdate();
   }
 
   setLang(lang) {
-    this.state.lang = lang;
+    // TODO: set language
     this.requestUpdate();
   }
 
@@ -103,17 +82,16 @@ export class TodoList extends LitElement {
   }
 
   loadStateFromLocalStorage() {
-    const saved_state = JSON.parse(localStorage.getItem('state')) ?? {};
+    // TODO: load state from LocalStorage
     this.state = {
       'hide_done': false,
       'lang': 'zh',
       'todos': [],
-      ...saved_state,
     };
   }
 
   saveStateToLocalStorage() {
-    localStorage.setItem('state', JSON.stringify(this.state));
+    // TODO: save state to LocalStorage
   }
 }
 
